8dda33b905b0c899674b650562578583ea39611e
Gerardo Pérez <gerard2perez@outlook.com>
2018-08-12 09:43:56 -0500

    fix: allow cluster

--- src/redbird.ts
+++ src/redbird.ts
@@ -2,7 +2,7 @@ import * as redbird from 'redbird';
 import { ModuleManager } from './module_manager';
 import * as os from 'os';
 const proxy = redbird({
-    // cluster: os.cpus().length,
+    cluster: os.cpus().length,
     port: 80,
     secure: false,
     bunyan: false,
@@ -24,14 +24,12 @@ interface Server {
 const Servers = new ModuleManager<Server>('proxy.yaml');
 Servers.set = (target:string, server:Server) =>{
     let sources:string[] = [].concat(server.host);
-    console.log(`Add: ${sources.join(',')} => ${target}`);
     for(const source of sources) {
         proxy.register(source, target, server.options); 
     }
 };
 Servers.remove = (target:string, server:Server) =>{
     let sources:string[] = [].concat(server.host);
-    console.log(`Remove: ${sources.join(',')} => ${target}`);
     for(const source of sources) {
         proxy.unregister(source, target);
     }

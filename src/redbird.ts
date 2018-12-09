effd21b473a5810693aab2d2c787c5358a6c8b56
Gerardo Pérez <gerard2perez@outlook.com>
2018-12-09 16:35:56 -0600

    feat: first release

--- src/redbird.ts
+++ src/redbird.ts
@@ -2,10 +2,11 @@ import * as redbird from 'redbird';
 import { ModuleManager } from './module_manager';
 import * as os from 'os';
 const proxy = redbird({
-    cluster: os.cpus().length,
+    // cluster: 1, 
+    //cluster: Math.ceil(os.cpus().length/4),
     port: 80,
     secure: false,
-    bunyan: false,
+    // bunyan: false,
     ssl: {
         port:443
     }
@@ -25,6 +26,7 @@ const Servers = new ModuleManager<Server>('proxy.yaml');
 Servers.set = (target:string, server:Server) =>{
     let sources:string[] = [].concat(server.host);
     for(const source of sources) {
+        // console.log(source, target, server.options);
         proxy.register(source, target, server.options); 
     }
 };
